7d4f73a6bfb2113b7dfed32369a6194277f6d706
Nick <distantfragment@gmail.com>
2022-05-23 20:51:11 +0300

    More tests for swan-marketplace contract

--- apps/solidity-contracts/tests/swan-marketplace.spec.ts
+++ apps/solidity-contracts/tests/swan-marketplace.spec.ts
@@ -39,7 +39,7 @@ describe("SwanNft", function () {
         await expect(deployedMarketplace.createListing(deployedNft.address, 1, -30)).to.be.reverted;
     });
 
-    it("createListing - creates listing and transfers nft", async () => {
+    it("createListing - creates listing, transfers nft and emits event", async () => {
         const [deployer, seller] = await ethers.getSigners();
 
         await deployedNft.createItem(seller.address, "the-uri");
@@ -49,6 +49,47 @@ describe("SwanNft", function () {
             .connect(seller)
             .createListing(deployedNft.address, 1, ethers.utils.parseEther("0.5"));
 
+        expect(result).to.emit(deployedMarketplace, "ListingCreated").withArgs(
+            seller.address,
+            deployedNft.address,
+            2,
+            ethers.utils.parseEther("0.5"),
+            1
+        );
         expect(await deployedNft.ownerOf(1)).to.equal(deployedMarketplace.address);
+    });
+
+    it("createListing - reverts if listing exists", async () => {
+        const [deployer, seller] = await ethers.getSigners();
+
+        await deployedNft.createItem(seller.address, "the-uri");
+        await deployedNft.connect(seller).approve(deployedMarketplace.address, 1);
+
+        await deployedMarketplace
+            .connect(seller)
+            .createListing(deployedNft.address, 1, ethers.utils.parseEther("0.5"));
+
+        await expect(deployedMarketplace
+            .connect(seller)
+            .createListing(deployedNft.address, 1, ethers.utils.parseEther("0.5"))).to.be.revertedWith(
+                "Token is already listed"
+        );
+    });
+
+    it("cancelListing - reverts if listing doesn't exist", async () => {
+        const [deployer, seller] = await ethers.getSigners();
+
+        await deployedNft.createItem(seller.address, "the-uri");
+        await deployedNft.connect(seller).approve(deployedMarketplace.address, 1);
+
+        await deployedMarketplace
+            .connect(seller)
+            .createListing(deployedNft.address, 1, ethers.utils.parseEther("0.5"));
+
+        await expect(deployedMarketplace
+            .connect(seller)
+            .cancelListing( 122)).to.be.revertedWith(
+            "Listing does not exist"
+        );
     });
 });
