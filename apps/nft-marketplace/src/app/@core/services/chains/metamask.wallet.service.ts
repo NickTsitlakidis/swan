906b151bd16bb4d13d470ff63bb995f77732351d
Nick Tsitlakidis <distantfragment@gmail.com>
2022-05-26 21:43:41 +0300

    library cleanup (#10)
    * library cleanup
    
    * Readme update and possible optimization of main github action
    
    * Fixing front end linting issues and adding lint process in gh action

--- apps/nft-marketplace/src/app/@core/services/chains/metamask.wallet.service.ts
+++ apps/nft-marketplace/src/app/@core/services/chains/metamask.wallet.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from "@angular/core";
-import detectEthereumProvider from "@metamask/detect-provider";
 import { WalletReadyState } from "@solana/wallet-adapter-base";
-import { Observable, from, map, first, switchMap, of } from "rxjs";
+import { Observable, from, map, switchMap, of, throwError } from "rxjs";
 import metamaskIcon from "../../../../assets/images/base64/metamask.base64.json";
 import { BlockChains } from "../../interfaces/blockchain.interface";
+import { ethers } from "ethers";
+import { isNil } from "lodash";
 
 @Injectable({
     providedIn: "root"
 })
 export class MetaMaskAuthService {
+    private _ethersProvider: ethers.providers.Web3Provider;
+
     public getWallets(): Observable<BlockChains> {
         const blockchain: BlockChains = {
             chain: {
@@ -30,41 +33,36 @@ export class MetaMaskAuthService {
     }
 
     public getPublicKey(): Observable<string | undefined> {
-        return from(detectEthereumProvider()).pipe(
-            switchMap(async (provider: any) => {
-                if (!provider) {
-                    throw new Error("Please install MetaMask");
-                }
-
-                return await provider.request({ method: "eth_requestAccounts" });
-            }),
-            first()
-        );
+        return this.getEthersProvider().pipe(
+            switchMap(() => {
+                return this._ethersProvider.getSigner().getAddress();
+            })
+        )
     }
 
     public onSelectWallet(): Observable<string | undefined> {
-        const observable = from(this.getPublicKey());
-        return observable.pipe(
-            map((data: any) => {
-                return data?.toString();
-            }),
-            first()
-        );
+        return this.getPublicKey();
     }
 
     public onSignMessage(msg: string): Observable<string> | undefined {
-        return from(detectEthereumProvider()).pipe(
-            switchMap(async (provider: any) => {
-                const signed = (await provider.request({
-                    method: "personal_sign",
-                    params: [msg, provider.selectedAddress]
-                })) as string;
-                return signed;
-            }),
-            map((signature: string) => {
-                return signature;
-            }),
-            first()
+        return this.getEthersProvider().pipe(
+            map((provider) => provider.getSigner()),
+            switchMap((signer) => {
+                return from(signer.signMessage(msg))
+            })
         );
+    }
+
+    private getEthersProvider(): Observable<ethers.providers.Web3Provider> {
+        const externalProvider = (window as any).ethereum;
+        if(!isNil(externalProvider)) {
+            if(!isNil(this._ethersProvider)) {
+                return of(this._ethersProvider);
+            }
+            this._ethersProvider = new ethers.providers.Web3Provider(externalProvider)
+            return from(this._ethersProvider.send("eth_requestAccounts", [])).pipe(map(() => this._ethersProvider));
+        } else {
+            return throwError(() => "Ethereum object is not added to window");
+        }
     }
 }
