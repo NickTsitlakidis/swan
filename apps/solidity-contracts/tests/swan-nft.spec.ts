5ce32436e8939d1394315cf9398021fd178f970d
Nick <distantfragment@gmail.com>
2022-06-09 23:01:35 +0300

    Updated swan-nft to allow create item function to be called by everyone.

--- apps/solidity-contracts/tests/swan-nft.spec.ts
+++ apps/solidity-contracts/tests/swan-nft.spec.ts
@@ -47,4 +47,21 @@ describe("SwanNft", function () {
             .to.emit(deployed, "Transfer")
             .withArgs("0x0000000000000000000000000000000000000000", signers[2].address, "3");
     });
+
+    it("createItem - can be called by everyone", async function () {
+        const SwanNft = await ethers.getContractFactory("SwanNft");
+        const deployed = await SwanNft.deploy();
+        await deployed.deployed();
+
+        const signers = await ethers.getSigners();
+
+        await expect(() => deployed.connect(signers[3]).createItem(signers[2].address, "the-uri")).to.changeTokenBalance(
+            deployed,
+            signers[2],
+            1
+        );
+
+        const uri = await deployed.tokenURI(1);
+        expect(uri).to.equal("the-uri");
+    });
 });
