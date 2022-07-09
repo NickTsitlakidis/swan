d0375e1f44049b83097ecdbdb4ec4554e8e9d4d2
Dimitris Agtzidis <agtzdimi@gmail.com>
2022-07-09 12:47:59 +0300

    Add test for getCollectionByUserId

--- apps/api/src/app/queries/collection-query-handler.spec.ts
+++ apps/api/src/app/queries/collection-query-handler.spec.ts
@@ -1,6 +1,8 @@
 import { CollectionViewRepository } from "../views/collection/collection-view-repository";
 import { CollectionQueryHandler } from "./collection-query-handler";
 import { getUnitTestingModule } from "../test-utils/test-modules";
+import { CollectionView } from "../views/collection/collection-view";
+import { ObjectID } from "mongodb";
 
 let repoMock: CollectionViewRepository;
 let handler: CollectionQueryHandler;
@@ -50,4 +52,46 @@ test("getUrlAvailability - returns false when url is found", async () => {
 
     expect(countSpy).toHaveBeenCalledWith("monkeys");
     expect(countSpy).toHaveBeenCalledTimes(1);
+});
+
+test("getCollectionByUserId - returns false when collection is found", async () => {
+    const countSpy = jest.spyOn(repoMock, "findByUserId").mockResolvedValue([]);
+
+    const result = await handler.getCollectionByUserId("user");
+    expect(result.length).toBe(0);
+
+    expect(countSpy).toHaveBeenCalledTimes(1);
+
+    expect(countSpy).toHaveBeenCalledWith("user");
+});
+
+test("getCollectionByUserId - returns false when collections is not found", async () => {
+    const collectionViews = [new CollectionView(), new CollectionView()];
+    collectionViews[0].id = new ObjectID().toHexString();
+    collectionViews[0].blockchainId = "block1";
+    collectionViews[0].categoryId = "cat1";
+    collectionViews[0].name = "collection1";
+    collectionViews[0].userId = "user";
+    collectionViews[1].id = new ObjectID().toHexString();
+    collectionViews[1].userId = "user";
+    collectionViews[1].blockchainId = "block2";
+    collectionViews[1].categoryId = "cat2";
+    collectionViews[1].name = "collection2";
+
+    const countSpy = jest.spyOn(repoMock, "findByUserId").mockResolvedValue(collectionViews);
+
+    const result = await handler.getCollectionByUserId("user");
+    expect(result.length).toBe(2);
+
+    expect(result[0].blockchainId).toBe("block1");
+    expect(result[0].categoryId).toBe("cat1");
+    expect(result[0].name).toBe("collection1");
+
+    expect(result[1].blockchainId).toBe("block2");
+    expect(result[1].categoryId).toBe("cat2");
+    expect(result[1].name).toBe("collection2");
+
+    expect(countSpy).toHaveBeenCalledTimes(1);
+
+    expect(countSpy).toHaveBeenCalledWith("user");
 });
