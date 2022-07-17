7e11d4ada140bc5c91bb403e79e1de9cd383f35e
Nick <distantfragment@gmail.com>
2022-07-17 20:12:23 +0300

    Update of fantom abi jsons

--- apps/nft-marketplace/src/app/@core/services/chains/metamask.service.ts
+++ apps/nft-marketplace/src/app/@core/services/chains/metamask.service.ts
@@ -5,7 +5,7 @@ import { ethers } from "ethers";
 import { isNil } from "lodash";
 import { CreateNft, MintTransaction } from "./nft";
 import { Injectable } from "@angular/core";
-import fantomSwanNft from "../../../../assets/evm-abi/fantom-swan-nft.json";
+import fantomSwanNft from "../../../../assets/evm-abi/fantom-mainnet-swan-nft.json";
 import { ChainsModule } from "./chains.module";
 import { TransactionResponse } from "@ethersproject/abstract-provider/src.ts";
 
