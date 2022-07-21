f1b998e29c86d35a4380c5db5f8e19a68b4e2236
Nick <distantfragment@gmail.com>
2022-07-21 18:24:36 +0300

    Initial work on back end nft retrieval

---
+++ apps/api/src/app/queries/nft-query-handler.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { ProfileNftDto } from "@nft-marketplace/common";
+import { UserWalletViewRepository } from "../views/user-wallet/user-wallet-view-repository";
+import { BlockchainRepository } from "../support/blockchains/blockchain-repository";
+
+@Injectable()
+export class NftQueryHandler {
+    constructor(
+        private _userWalletRepository: UserWalletViewRepository,
+        private _blockchainRepository: BlockchainRepository
+    ) {}
+
+    async getByUserId(userId: string): Promise<Array<ProfileNftDto>> {
+        const userWallets = await this._userWalletRepository.findByUserId(userId);
+
+        const chains = await this._blockchainRepository.findByIds(userWallets.map((wallet) => wallet.blockchainId));
+    }
+}
