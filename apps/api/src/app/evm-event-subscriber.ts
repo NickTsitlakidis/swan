ce5dcd365ca21a5803f6bab58634be6b4458f9b2
Nick Tsitlakidis <distantfragment@gmail.com>
2022-11-28 16:15:55 +0200

    Delete evm-event-subscriber.ts

--- apps/api/src/app/evm-event-subscriber.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from "@nestjs/common";
-import { ContractFactory } from "@swan/contracts";
-
-@Injectable()
-export class EvmEventSubscriber {
-    constructor(private readonly _contractFactory: ContractFactory) {}
-}
