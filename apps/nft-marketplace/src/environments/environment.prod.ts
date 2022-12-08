cbb552e57a120e2a18a4e44538c72a4bd81e31b8
Dimitrios Agtzidis <agtzdimi@gmail.com>
2022-12-08 23:31:07 +0200

    change domain name in production build

--- apps/nft-marketplace/src/environments/environment.prod.ts
+++ apps/nft-marketplace/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
     production: true,
     applicationId: "139dbf30-c92f-4305-8002-a1758894aa12",
     applicationSecret: "NkxsnKEnkBjUs7JGBy5k",
-    serverUrl: "http://localhost:3310/api",
+    serverUrl: "https://swan-marketplace.eu/api",
     lambdaS3Uri: "https://yglp3mwypvr4fh7aenwc3jxh6e0xdfvt.lambda-url.eu-central-1.on.aws/"
 };
